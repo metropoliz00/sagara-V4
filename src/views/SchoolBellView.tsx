98309b7a40002aea3da35070dbc284a4c647dd59
DEDY MEYGA SAPUTRA <119374110+metropoliz00@users.noreply.github.com>
2026-02-26 05:57:30 +0700

    feat: Remove unused School Bell feature
    Removes the School Bell component and related types and dependencies. This feature was not being actively used and is no longer required.

--- src/views/SchoolBellView.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SchoolBell from '../components/SchoolBell';
-
-const SchoolBellView: React.FC = () => {
-  return <SchoolBell />;
-};
-
-export default SchoolBellView;
